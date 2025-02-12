13030ef42e1d6531b5a5cbb4a06c421598f3fc21
Vitaly_GDS <vitalygds@gmail.com>
2025-02-12 19:20:45 +0300

    add api proj

--- src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServer")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6e3fd9e9c2cb7eb2da3412c6569f4364f1deaf")]
-[assembly: System.Reflection.AssemblyProductAttribute("IdentityServer")]
-[assembly: System.Reflection.AssemblyTitleAttribute("IdentityServer")]
+[assembly: System.Reflection.AssemblyProductAttribute("Api")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
