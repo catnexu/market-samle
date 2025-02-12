5c2d5c242fc1796f1681af9180558b159b90e5e6
Vitaly_GDS <vitalygds@gmail.com>
2025-02-12 20:52:15 +0300

    launch setup

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using IdentityModel.Client;
+
+namespace Client;
+
+internal class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        var client = new HttpClient();
+        var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+        if (disco.IsError)
+        {
+            Console.WriteLine(disco.Error);
+            Console.WriteLine(disco.Exception);
+            return 1;
+        }
+        
+        // request token
+        var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+        {
+            Address = disco.TokenEndpoint,
+            ClientId = "client",
+            ClientSecret = "secret",
+            Scope = "api1"
+        });
+
+        if (tokenResponse.IsError)
+        {
+            Console.WriteLine(tokenResponse.Error);
+            Console.WriteLine(tokenResponse.ErrorDescription);
+            return 1;
+        }
+
+        Console.WriteLine(tokenResponse.AccessToken);
+
+
+        // call api
+        var apiClient = new HttpClient();
+        apiClient.SetBearerToken(tokenResponse.AccessToken!); // AccessToken is always non-null when IsError is false
+
+        var response = await apiClient.GetAsync("https://localhost:6001/identity");
+        if (!response.IsSuccessStatusCode)
+        {
+            Console.WriteLine(response.StatusCode);
+            return 1;
+        }
+
+        var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync()).RootElement;
+        Console.WriteLine(JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true }));
+        return 0;
+
+    }
+}
