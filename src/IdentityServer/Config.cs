c5bd1969f3e845a4803a304a24cde32bb9baf638
Vitaly_GDS <vitalygds@gmail.com>
2025-02-12 20:45:59 +0300

    client

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -6,19 +6,19 @@ public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
-        { 
+        {
             new IdentityResources.OpenId()
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new ApiScope(name: "api1", displayName: "My API") 
+            new ApiScope(name: "api1", displayName: "My API")
         };
 
     public static IEnumerable<Client> Clients =>
         new Client[]
-    
+
         {
             new Client
             {
@@ -34,8 +34,7 @@ public static class Config
                 },
 
                 // scopes that client has access to
-                AllowedScopes = { "api1" }
+                AllowedScopes = {"api1"}
             }
         };
-
 }
