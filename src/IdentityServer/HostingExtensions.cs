a4f0179242b101130d5f18a754e4f7bcbc2ded7f
Vitaly_GDS <vitalygds@gmail.com>
2025-02-17 12:34:13 +0300

    db migration

--- src/IdentityServer/HostingExtensions.cs
+++ src/IdentityServer/HostingExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace IdentityServer;
@@ -6,17 +7,22 @@ internal static class HostingExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
-        // uncomment if you want to add a UI
         builder.Services.AddRazorPages();
 
-        builder.Services.AddIdentityServer(options =>
+        var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+        const string connectionString = @"Data Source=Duende.IdentityServer.Quickstart.EntityFramework.db";
+
+        builder.Services.AddIdentityServer()
+            .AddConfigurationStore(options =>
             {
-                // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
-                options.EmitStaticAudienceClaim = true;
+                options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
             })
-            .AddInMemoryIdentityResources(Config.IdentityResources)
-            .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients)
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
             .AddTestUsers(TestUsers.Users);
 
         return builder.Build();
