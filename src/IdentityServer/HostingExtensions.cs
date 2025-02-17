f8fd55bb43962db43558a0366324f134bae42ded
Vitaly_GDS <vitalygds@gmail.com>
2025-02-17 12:48:52 +0300

    db creation

--- src/IdentityServer/HostingExtensions.cs
+++ src/IdentityServer/HostingExtensions.cs
@@ -1,3 +1,5 @@
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -36,6 +38,8 @@ internal static class HostingExtensions
         {
             app.UseDeveloperExceptionPage();
         }
+        
+        InitializeDatabase(app);
 
         // uncomment if you want to add a UI
         app.UseStaticFiles();
@@ -48,5 +52,46 @@ internal static class HostingExtensions
         app.MapRazorPages().RequireAuthorization();
 
         return app;
+    }
+
+
+    private static void InitializeDatabase(IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope())
+        {
+            serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+            var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            context.Database.Migrate();
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Config.Clients)
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in Config.IdentityResources)
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+
+            if (!context.ApiScopes.Any())
+            {
+                foreach (var resource in Config.ApiScopes)
+                {
+                    context.ApiScopes.Add(resource.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+        }
     }
 }
