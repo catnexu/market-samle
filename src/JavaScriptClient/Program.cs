1daafac80552518e9c8c277b52dce7220db98bc8
Vitaly_GDS <vitalygds@gmail.com>
2025-02-20 09:47:52 +0300

    client impl

--- src/JavaScriptClient/Program.cs
+++ src/JavaScriptClient/Program.cs
@@ -1,6 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Duende.Bff.Yarp;
 
-app.MapGet("/", () => "Hello World!");
+namespace JavaScriptClient;
 
-app.Run();
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+      
+        builder.Services
+            .AddBff()
+            .AddRemoteApis();
+
+        builder.Services
+            .AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+                options.DefaultSignOutScheme = "oidc";
+            })
+            .AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = "https://localhost:5001";
+                options.ClientId = "bff";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+                options.Scope.Add("api1");
+                options.Scope.Add("offline_access");
+                options.SaveTokens = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.MapInboundClaims = false;
+            });
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+
+        app.UseBff();
+
+        app.UseAuthorization();
+        app.MapBffManagementEndpoints();
+
+        app.Run();
+    }
+}
